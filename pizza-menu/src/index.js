a09138dd691d29d4b41157bda71c33e3f38e74ef
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-02-13 21:59:52 -0500

    first program

--- pizza-menu/src/index.js
+++ pizza-menu/src/index.js
@@ -1,17 +1,151 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDom from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App() {
+  return (
+    <div className="container">
+      <Headers />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Headers() {
+  const style = {};
+
+  return (
+    <header className="header">
+      <h1 style={{ style }}>Get Your Pizza On The Go.</h1>
+    </header>
+  );
+}
+function Menu() {
+  const pizzas = pizzaData;
+  //   const pizzas = [];
+  const numPizzas = pizzas.length;
+  return (
+    <main className="menu">
+      <h2>Our Pizza Menu</h2>
+      <p>Come by and test our delicious Pizza Menu</p>
+      {numPizzas > 0 ? (
+        <React.Fragment>
+          <ul className="pizzas">
+            {pizzaData.map((pizza) => (
+              <Pizza pizzaObject={pizza} key={pizza.name} />
+            ))}
+            )
+          </ul>
+        </React.Fragment>
+      ) : (
+        <p>We are still working on our menu, please come visit us later</p>
+      )}
+    </main>
+  );
+}
+
+function Pizza({ pizzaObject }) {
+  console.log(pizzaObject);
+  //   if (pizzaObject.soldOut) {
+  //     return null;
+  //   }
+  return (
+    <li className={`pizza ${pizzaObject.soldOut ? "sold-out" : ""}`}>
+      <img src={pizzaObject.photoName} alt={pizzaObject.name} />
+      <div>
+        <h3>{pizzaObject.name}</h3>
+        <p>{pizzaObject.ingredients}</p>
+        {pizzaObject.soldOut ? (
+          <span>SOLD OUT</span>
+        ) : (
+          <span>{pizzaObject.price}</span>
+        )}
+        {/* <span>{pizzaObject.soldOut ? "SOLD OUT" : pizzaObject.price}</span> */}
+      </div>
+    </li>
+  );
+}
+
+function Footer() {
+  const hours = new Date().getHours();
+  const openHours = 12;
+  const closeHours = 22;
+  const isOpen = hours >= openHours && hours <= closeHours;
+
+  return (
+    <footer className="footer">
+      {isOpen ? (
+        <Order closeHours={closeHours} openHours={openHours} />
+      ) : (
+        <p>
+          We are happy to welcome you between {openHours}:00 and {closeHours}:00
+        </p>
+      )}
+    </footer>
+  );
+}
+
+function Order({ closeHours, openHours }) {
+  return (
+    <div>
+      <p>
+        We are Open from {openHours}:00 to {closeHours}:00. Come visit us
+        online!
+      </p>
+      <button className="btn">Open</button>
+    </div>
+  );
+}
+
+const root = ReactDom.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
